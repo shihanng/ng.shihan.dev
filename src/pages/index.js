47aa7351e0df6f5b2fe9111a9e7a6062d9c0382b
Shi Han NG <shihanng@gmail.com>
2020-12-24 15:43:23 +0900

    Add link to writing in index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
+import { Link } from "gatsby"
 import React from "react"
 import Header from "../components/header"
 
 export default function Home() {
   return (
     <>
+      <Link to="/writing/">Writing</Link>
       <Header text="Home" />
     </>
   )
