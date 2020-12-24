22c249c284f601dc8eb423dce9b05ee4e334b803
Shi Han NG <shihanng@gmail.com>
2020-12-24 15:53:13 +0900

    Add tailwind
    https://www.gatsbyjs.com/docs/how-to/styling/tailwind-css/

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 import { Link } from "gatsby"
 import React from "react"
+import tw from "twin.macro"
 import Header from "../components/header"
+
+const Button = tw.button`
+  bg-blue-500 hover:bg-blue-800 text-white p-2 rounded
+`
 
 export default function Home() {
   return (
     <>
       <Link to="/writing/">Writing</Link>
       <Header text="Home" />
+      <Button>Click me</Button>
     </>
   )
 }
