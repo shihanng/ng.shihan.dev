21242af61e50f0ed3c0c2bca09064063a1fdc332
Shi Han NG <shihanng@gmail.com>
2020-12-24 15:26:55 +0900

    Add writing page

---
+++ src/pages/writing.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Writing() {
+  return (
+    <div style={{ color: `teal` }}>
+      <h1>Writing</h1>
+      <p>Some articles written by me</p>
+    </div>
+  )
+}
