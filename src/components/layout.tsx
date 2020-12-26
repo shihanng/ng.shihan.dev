1dc357104c50d3500d0b7c8790651560b5202b15
Shi Han NG <shihanng@gmail.com>
2020-12-26 20:44:08 +0900

    Add NavBar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,23 @@
 import { graphql, Link, useStaticQuery } from "gatsby"
 import React from "react"
 import { Helmet } from "react-helmet"
-import tw, { GlobalStyles } from "twin.macro"
+import tw, { GlobalStyles, styled } from "twin.macro"
 import Contact from "../components/contact"
 
-const Container = tw.div`flex flex-col h-screen text-gray-800`
-const Page = tw.div`flex-1 mx-5 md:mx-auto font-sans prose prose-lg py-7 max-w-4xl`
-const Li = tw.li`inline-block ml-3`
-const Ul = tw.ul`list-none float-right`
+const Container = tw.div`font-sans flex flex-col h-screen text-gray-800 max-w-full mx-5 md:mx-auto md:max-w-3xl`
+const Page = tw.div`flex-1 prose prose-lg py-7 max-w-max`
+const Ul = tw.ul`flex flex-row`
 const Header = tw.header`text-center py-5 font-serif text-2xl`
 const Footer = tw.footer`text-center py-5 font-serif text-sm`
+const Li = styled.li`
+  ${tw`ml-5 font-semibold text-gray-500`}
+  & .hello {
+    ${tw`text-purple-700`}
+  }
+  &:hover {
+    ${tw`text-gray-800`}
+  }
+`
 
 interface ListLinkProps {
   to: string
@@ -17,9 +25,27 @@ interface ListLinkProps {
 
 const ListLink: React.FC<ListLinkProps> = ({ to, children }) => (
   <Li>
-    <Link to={to}>{children}</Link>
+    <Link
+      to={to}
+      getProps={({ isCurrent }) => (isCurrent ? { className: "hello" } : {})}
+    >
+      {children}
+    </Link>
   </Li>
 )
+
+const Nav = tw.nav`flex justify-end`
+
+const NavBar: React.FC = () => {
+  return (
+    <Nav>
+      <Ul>
+        <ListLink to="/">Home</ListLink>
+        <ListLink to="/writing/">Writing</ListLink>
+      </Ul>
+    </Nav>
+  )
+}
 
 const Layout: React.FC = ({ children }) => {
   const data = useStaticQuery(
@@ -40,13 +66,8 @@ const Layout: React.FC = ({ children }) => {
       <Container>
         <GlobalStyles />
         <Header>{data.site.siteMetadata.title}</Header>
-        <Page>
-          <Ul>
-            <ListLink to="/">Home</ListLink>
-            <ListLink to="/writing/">Writing</ListLink>
-          </Ul>
-          {children}
-        </Page>
+        <NavBar />
+        <Page>{children}</Page>
         <Footer>
           <div>
             &copy; Shi Han NG |{" "}
