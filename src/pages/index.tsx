405e4eafce7068b6f38d3d5259afd239dde7c44e
Shi Han NG <shihanng@gmail.com>
2021-01-02 15:55:38 +0900

    Update Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,34 @@
 import React from "react"
+import tw from "twin.macro"
 import Layout from "../components/Layout"
+
+const P = tw.p`mb-3`
 
 export default function Home() {
   return (
     <Layout>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Pulvinar elementum
-        integer enim neque volutpat. At in tellus integer feugiat scelerisque
-        varius morbi. Nunc sed blandit libero volutpat. Ac ut consequat semper
-        viverra nam libero justo laoreet. In iaculis nunc sed augue lacus
-        viverra. Et tortor consequat id porta. Tortor at auctor urna nunc.
-        Malesuada nunc vel risus commodo viverra maecenas accumsan lacus vel.
-        Vel risus commodo viverra maecenas accumsan lacus vel. Tincidunt nunc
-        pulvinar sapien et ligula. Faucibus scelerisque eleifend donec pretium
-        vulputate sapien nec sagittis. Aenean et tortor at risus viverra
-        adipiscing. Ut consequat semper viverra nam libero justo. Mi proin sed
-        libero enim. Velit egestas dui id ornare arcu odio ut sem nulla.
-        Tincidunt augue interdum velit euismod in. Ultrices in iaculis nunc sed
-        augue lacus viverra.
-      </p>
+      <P>
+        Software engineer based in Tokyo. I have a background in numerical
+        simulations focusing on granular particle simulation and computational
+        fluid dynamics. After graduate school, I started my software engineering
+        career working in the backend domain of web development. I mainly worked
+        with the Go programming language. Then slowly expanded my knowledge to
+        the infrastructure domains (AWS, Terraform, etc.). Now I am also working
+        in the frontend realm with TypeScript and React.js. I am also exposing
+        myself to Python and the Django framework at the same time.
+      </P>
+      <P>
+        I enjoy being a generalist and love to learn new things from various
+        stacks of technologies: from new programming languages/paradigms,
+        monolithic/microservices architectures, distributed systems, etc.
+      </P>
+      <P>
+        I strongly believe in automation, be it automated code testings/lintings
+        or continuous integrations/deliveries (CI/CD). I care about the software
+        maintainability and have no ego and emotional attachments to the code
+        that I wrote. I find myself learning most efficiently by giving and
+        receiving code reviews.
+      </P>
     </Layout>
   )
 }
