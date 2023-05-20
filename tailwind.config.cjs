89281475ae00b41eb2d612e535675cc79a291ee4
Shi Han NG <shihanng@gmail.com>
2023-05-20 20:51:45 +0900

    Design simple index page
    With preline

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,20 @@
+const preline = require("preline/plugin.js");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: [
+    "./public/**/*.astro",
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+    "node_modules/preline/dist/*.js",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "bright-gray": {
+          DEFAULT: "#393E46",
+        },
+      },
+    },
+  },
+  plugins: [preline],
+};
